5a1fbbeb9d760b3f587bb220c8139b62ff6850a0
Charlie <carlos.tambascia@gmail.com>
2020-04-20 11:55:28 -0300

    Adding firebase

--- lib/pages/coach_register_screen.dart
+++ lib/pages/coach_register_screen.dart
@@ -1,6 +1,8 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:letsrun/plugins/constants.dart';
+import 'package:nice_button/NiceButton.dart';
 
 class CoachRegisterScreen extends StatefulWidget {
   static String id = 'coach_register_screen';
@@ -10,16 +12,22 @@ class CoachRegisterScreen extends StatefulWidget {
 }
 
 class _CoachRegisterScreen extends State<CoachRegisterScreen> {
+  String email;
+  String password;
+  String name;
+  String surname;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         centerTitle: true,
         title: Text('Registro'),
       ),
-      backgroundColor: Color(0xFF8185E2),
-      body: Center(
+      backgroundColor: Colors.white,
+      body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             AvatarGlow(
               endRadius: 90,
@@ -33,11 +41,66 @@ class _CoachRegisterScreen extends State<CoachRegisterScreen> {
                   shape: CircleBorder(),
                   child: CircleAvatar(
                     backgroundColor: Colors.grey[100],
-                    child: FlutterLogo(
-                      size: 50.0,
-                    ),
+                    child: Image.asset('assets/img/illustration.png'),
                     radius: 50.0,
                   )),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              keyboardType: TextInputType.text,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                name = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Nombre'),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              keyboardType: TextInputType.text,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                surname = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Apellido'),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                email = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Email'),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Contrasena'),
+            ),
+            SizedBox(height: 25.0),
+            NiceButton(
+              padding: EdgeInsets.all(15.0),
+              radius: 30.0,
+              elevation: 5,
+              mini: false,
+              icon: Icons.person,
+              onPressed: () {},
+              text: 'Registrame',
+              background: Theme.of(context).primaryColor,
             ),
           ],
         ),
