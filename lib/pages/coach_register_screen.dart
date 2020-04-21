ec89a03b814456f4400d5b6924298cf5432cf933
Charlie <carlos.tambascia@gmail.com>
2020-04-20 23:12:43 -0300

    small improvements

--- lib/pages/coach_register_screen.dart
+++ lib/pages/coach_register_screen.dart
@@ -20,6 +20,7 @@ class _CoachRegisterScreen extends State<CoachRegisterScreen> {
   String name;
   String surname;
   File profilePicture;
+  bool _passwordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -32,86 +33,116 @@ class _CoachRegisterScreen extends State<CoachRegisterScreen> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              GestureDetector(
-                onTap: () => showDialog(context: context, builder: (_) => askForSource()),
-                child: AvatarGlow(
-                  endRadius: 90,
-                  duration: Duration(seconds: 2),
-                  glowColor: Colors.white24,
-                  repeat: true,
-                  repeatPauseDuration: Duration(seconds: 2),
-                  startDelay: Duration(seconds: 1),
-                  child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[100],
-                        child: Image.asset('assets/img/cam2.png'),
-                        radius: 50.0,
-                      )),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () => showDialog(context: context, builder: (_) => askForSource()),
+                  child: AvatarGlow(
+                    endRadius: 90,
+                    duration: Duration(seconds: 2),
+                    glowColor: Colors.white24,
+                    repeat: true,
+                    repeatPauseDuration: Duration(seconds: 2),
+                    startDelay: Duration(seconds: 1),
+                    child: Material(
+                        elevation: 8.0,
+                        shape: CircleBorder(),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Image.asset('assets/img/cam2.png'),
+                          radius: 50.0,
+                        )),
+                  ),
                 ),
-              ),
-              TextField(
-                style: TextStyle(color: Colors.black),
-                keyboardType: TextInputType.text,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  name = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: 'Nombre'),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.black),
-                keyboardType: TextInputType.text,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  surname = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: 'Apellido'),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.black),
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: 'Email'),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.black),
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: 'Contrasena'),
-              ),
-              SizedBox(height: 25.0),
-              NiceButton(
-                padding: EdgeInsets.all(15.0),
-                radius: 30.0,
-                elevation: 5,
-                mini: false,
-                icon: Icons.person,
-                onPressed: () {},
-                text: 'Registrame',
-                background: Theme.of(context).primaryColor,
-              ),
-            ],
+                TextField(
+                  style: TextStyle(color: Colors.black),
+                  keyboardType: TextInputType.text,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  //Todo: add validation for the fields
+                  decoration: kTextFieldDecoration.copyWith(hintText: 'Nombre Completo'),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.black),
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(hintText: 'Email'),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.black),
+                  keyboardType: TextInputType.text,
+                  obscureText: _passwordVisible ? true : false,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Contrasena',
+                    suffixIcon: IconButton(
+                        icon: Icon(
+                          _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                          semanticLabel: _passwordVisible ? 'hide password' : 'show password',
+                        ),
+                        onPressed: () => setState(() => _passwordVisible ^= true)),
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.camera_alt),
+                        title: Text('Adjunta tu certificado'),
+                        subtitle: Text('Para comprobar que eres un entrenador por favor adjunta tu certificacion.'),
+                      ),
+                      ButtonBar(
+                        children: <Widget>[
+                          FlatButton(
+                            child: Text(
+                              'Adjuntar',
+                              style: TextStyle(color: Theme.of(context).primaryColor),
+                            ),
+                            onPressed: () {
+                              /* ... */
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                NiceButton(
+                  padding: EdgeInsets.all(15.0),
+                  radius: 30.0,
+                  elevation: 5,
+                  mini: false,
+                  icon: Icons.person,
+                  onPressed: () {},
+                  text: 'Registrame',
+                  background: Theme.of(context).primaryColor,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -123,14 +154,14 @@ class _CoachRegisterScreen extends State<CoachRegisterScreen> {
       title: Text('De donde quieres tomar la fotografia?'),
       actions: <Widget>[
         FlatButton(
-          onPressed: () => pickImage(ImageSource.camera),
+          onPressed: () => _pickImage(ImageSource.camera),
           child: Text(
             'Camara',
             style: TextStyle(color: Theme.of(context).primaryColor),
           ),
         ),
         FlatButton(
-          onPressed: () => pickImage(ImageSource.gallery),
+          onPressed: () => _pickImage(ImageSource.gallery),
           child: Text(
             'Galeria',
             style: TextStyle(color: Theme.of(context).primaryColor),
@@ -141,7 +172,7 @@ class _CoachRegisterScreen extends State<CoachRegisterScreen> {
     );
   }
 
-  Future<void> pickImage(ImageSource source) async {
+  Future<void> _pickImage(ImageSource source) async {
     File selected = await ImagePicker.pickImage(source: source);
     setState(() => profilePicture = selected);
   }
