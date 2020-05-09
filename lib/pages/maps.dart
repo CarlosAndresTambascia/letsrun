3d0cd74dc8609ba63ed1cf839b174a2672a8c98c
Charlie <carlos.tambascia@gmail.com>
2020-05-08 22:06:57 -0300

    Making the map work.

--- lib/pages/maps.dart
+++ lib/pages/maps.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:letsrun/models/location.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class Maps extends StatefulWidget {
   static String id = 'maps';
+
   @override
   _MapsState createState() => _MapsState();
 }
@@ -14,63 +16,70 @@ class _MapsState extends State<Maps> {
   double latitude;
   double longitude;
   Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = LatLng(45.512563, -122.677433);
+  GoogleMapController _googleMapController;
+  static LatLng _center = LatLng(45.512563, -122.677433);
   final Set<Marker> _markers = {};
+  final Set<Marker> _myLocationMarker = {};
   LatLng _lastMapPosition = _center;
   MapType _currentMapType = MapType.normal;
-
-  @override
-  void initState() {
-    _getCurrentLocation();
-    super.initState();
-  }
+  bool _loading = false;
+  final String _startingPositionMsg = 'Principio del recorrido';
+  final String _finalPositionMsg = 'Final del recorrido';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Selecciona la ruta'),
-          backgroundColor: Theme.of(context).accentColor,
-        ),
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
-              mapType: _currentMapType,
-              markers: _markers,
-              onCameraMove: _onCameraMove,
+    return ModalProgressHUD(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Selecciona la ruta'),
+            centerTitle: true,
+            backgroundColor: Theme.of(context).primaryColor,
+            iconTheme: IconThemeData(
+              color: Colors.white, //change your color here
             ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: Column(
-                  children: <Widget>[
-                    button(_onMapTypeButtonPressed, Icons.map),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    //button(_onAddMarkerButtonPressed, Icons.add_location),
-                  ],
+          ),
+          body: Stack(
+            children: <Widget>[
+              GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(target: _center, zoom: 15.0),
+                mapType: _currentMapType,
+                markers: _markers,
+                onCameraMove: _onCameraMove,
+                onTap: _handleTap,
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Column(
+                    children: <Widget>[
+                      button(_getCurrentLocation, Icons.my_location, 'myLocaiton'),
+                      SizedBox(height: 16.0),
+                      button(_clearMarker, Icons.delete, 'erase'),
+                      SizedBox(height: 16.0),
+                      button(_getCurrentLocation, Icons.check_circle, 'done'),
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
+      inAsyncCall: _loading,
     );
   }
 
-  Future<void> _getCurrentLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
+  _clearMarker() {
+    setState(() {
+      _markers.clear();
+    });
   }
 
   void _onMapCreated(GoogleMapController controller) {
+    _googleMapController = controller;
     _controller.complete(controller);
   }
 
@@ -78,28 +87,65 @@ class _MapsState extends State<Maps> {
     _lastMapPosition = position.target;
   }
 
-  Widget button(Function function, IconData icon) {
+  Widget button(Function function, IconData icon, String heroTag) {
     return FloatingActionButton(
       onPressed: function,
       materialTapTargetSize: MaterialTapTargetSize.padded,
-      backgroundColor: Theme.of(context).accentColor,
+      backgroundColor: Theme.of(context).primaryColor,
       child: Icon(icon, size: 36.0),
+      heroTag: heroTag,
     );
   }
 
-  _onMapTypeButtonPressed() {
-    setState(() => _currentMapType = _currentMapType == MapType.normal ? MapType.satellite : MapType.normal);
+  _handleTap(LatLng point) {
+    bool hasStartingPosition = false;
+    bool hasFinalPosition = false;
+
+    Marker startingMarker = Marker(
+      markerId: MarkerId(_startingPositionMsg),
+      position: point,
+      infoWindow: InfoWindow(
+        title: _startingPositionMsg,
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    );
+
+    Marker finalMarker = Marker(
+      markerId: MarkerId(_finalPositionMsg),
+      position: point,
+      infoWindow: InfoWindow(
+        title: _finalPositionMsg,
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+    );
+
+    hasStartingPosition = _markers.toString().contains(_startingPositionMsg);
+    hasFinalPosition = _markers.toString().contains(_finalPositionMsg);
+
+    if (!hasStartingPosition) {
+      setState(() => _markers.add(startingMarker));
+    } else if (!hasFinalPosition) {
+      setState(() => _markers.add(finalMarker));
+    } else {
+      return null;
+    }
   }
 
-  _onAddMarkerButtonPressed() {
+  _getCurrentLocation() async {
+    setState(() => _loading = true);
+    var currentLocation = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     setState(() {
+      _markers.clear();
+      _center = LatLng(currentLocation.latitude, currentLocation.longitude);
       _markers.add(
-        Marker(
-            markerId: MarkerId(_lastMapPosition.toString()),
-            position: _lastMapPosition,
-            infoWindow: InfoWindow(title: 'Esta es la posicion inicial'),
-            icon: BitmapDescriptor.defaultMarker),
-      );
+          Marker(position: _center, infoWindow: InfoWindow(title: 'Tu ubicacion'), markerId: MarkerId('myLocaiton')));
+      _loading = false;
+      _googleMapController.moveCamera(CameraUpdate.newLatLng(_center));
     });
+  }
+
+  _removeMarker(MarkerId markerId) {
+    var toRemove = _markers.firstWhere((marker) => marker.markerId == markerId);
+    _markers.remove(toRemove);
   }
 }
