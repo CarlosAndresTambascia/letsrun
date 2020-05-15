f60d9df47ea8d9037d526d5e78ed666656b34839
ignaciocasales <casales.ignacio@gmail.com>
2020-05-15 01:13:11 -0300

    Fix subscribe button & better componentization

--- lib/pages/news_screen.dart
+++ lib/pages/news_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_card/animated_card.dart';
 import 'package:animated_card/animated_card_direction.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:letsrun/models/post.dart';
 import 'package:letsrun/pages/home_screen.dart';
 import 'package:letsrun/pages/post_map.dart';
 import 'package:letsrun/services/firestoreManagement.dart';
@@ -15,14 +16,12 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> {
-  final double _circleRadius = 75.0;
-  final double _circleBorderWidth = 5.0;
   Stream<QuerySnapshot> postsSnapshots;
-  List assistants;
   String currentUserFullName = HomeScreen.currentAppUser.fullName;
 
   @override
   void initState() {
+    super.initState();
     postsSnapshots = FirestoreManagement().getPostsSnapshots();
   }
 
@@ -44,182 +43,208 @@ class _NewsState extends State<News> {
           return ListView.builder(
             itemCount: snapshot.data.documents.length,
             itemBuilder: (context, index) {
-              var currentPost = snapshot.data.documents[index].data;
-              assistants = currentPost['assistants'];
-              String pid = currentPost['pid'];
-              double latitudeStarting = currentPost['latitudeStarting'];
-              double longitudeStarting = currentPost['longitudeStarting'];
-              double latitudeEnd = currentPost['latitudeEnd'];
-              double longitudeEnd = currentPost['longitudeEnd'];
-              return AnimatedCard(
-                direction: AnimatedCardDirection.left,
-                initDelay: Duration(milliseconds: 0),
-                duration: Duration(seconds: 1),
-                curve: Curves.bounceOut,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Card(
-                    color: Colors.white70,
-                    elevation: 5,
-                    child: Container(
-                      padding: EdgeInsets.all(8.0),
-                      height: 280.0,
-                      child: Column(
-                        children: <Widget>[
-                          Stack(alignment: Alignment.topCenter, children: <Widget>[
-                            Padding(
-                              child: SizedBox(
-                                width: 350.0,
-                                height: 180.0,
-                                child: Card(
-                                  child: Padding(
-                                    padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
-                                    child: ReadMoreText(
-                                      currentPost['description'],
-                                      //HomeScreen.currentAppUser.email,
-                                      trimLines: 5,
-                                      colorClickableText: Theme.of(context).primaryColor,
-                                      trimMode: TrimMode.Line,
-                                      trimCollapsedText: '...ver mas',
-                                      trimExpandedText: ' ver menos',
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              padding: EdgeInsets.only(top: _circleRadius / 2.0),
-                            ),
-                            Container(
-                              width: _circleRadius,
-                              height: _circleRadius,
-                              decoration: ShapeDecoration(shape: CircleBorder(), color: Colors.white),
-                              child: Padding(
-                                padding: EdgeInsets.all(_circleBorderWidth),
-                                child: DecoratedBox(
-                                  decoration: ShapeDecoration(
-                                    shape: CircleBorder(),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(
-                                        currentPost['profilePicUrl'],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            )
-                          ]),
-                          SizedBox(
-                            height: 8.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Column(
-                                children: <Widget>[
-                                  SizedBox(
-                                    height: 35.0,
-                                    width: 100.0,
-                                    child: Material(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(25.0),
-                                      ),
-                                      elevation: 5.0,
-                                      color: Colors.white,
-                                      child: Container(
-                                        child: Padding(
-                                          padding: EdgeInsets.only(left: 25.0),
-                                          child: Row(
-                                            children: <Widget>[
-                                              Column(
-                                                children: <Widget>[
-                                                  LikeButton(
-                                                    likeCount: assistants.length,
-                                                    likeBuilder: (bool isLiked) => Icon(
-                                                      Icons.directions_run,
-                                                      color: isLiked || assistants.contains(currentUserFullName)
-                                                          ? Colors.deepPurpleAccent
-                                                          : Colors.grey,
-                                                    ),
-                                                    onTap: (isLiked) async => await _addAssistants(pid, isLiked),
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(
-                                width: 5.0,
-                              ),
-                              SizedBox(
-                                height: 35.0,
-                                width: 100.0,
-                                child: Material(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(25.0),
-                                  ),
-                                  elevation: 5.0,
-                                  color: Colors.white,
-                                  child: InkWell(
-                                    onTap: () => _goToMapWithCoordinates(
-                                        context, latitudeStarting, longitudeStarting, latitudeEnd, longitudeEnd),
-                                    child: Container(
-                                      child: Padding(
-                                        padding: EdgeInsets.only(left: 35.0, top: 3.0),
-                                        child: Row(
-                                          children: <Widget>[
-                                            Column(
-                                              children: <Widget>[
-                                                Icon(
-                                                  Icons.location_on,
-                                                  color: Colors.grey,
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              );
+              return PostWidget(post: Post.fromData(snapshot.data.documents[index].data), currentUserFullName: currentUserFullName);
             },
           );
         }
       },
     );
   }
+}
 
-  Future<bool> _addAssistants(String pid, bool val) async {
-    if (!assistants.contains(currentUserFullName)) {
-      assistants.add(currentUserFullName);
-      FirestoreManagement().addListener(pid, assistants);
-    } else {
-      assistants.remove(currentUserFullName);
-      FirestoreManagement().removeListener(pid, assistants);
-    }
-    return !val;
+class PostWidget extends StatelessWidget {
+  PostWidget({Key key, @required this.post, @required this.currentUserFullName})
+      : _isSubscribed = post.assistants.contains(currentUserFullName),
+        _subscribedCount = post.assistants.length,
+        super(key: key);
+
+  final Post post;
+  final String currentUserFullName;
+  final bool _isSubscribed;
+  final int _subscribedCount;
+  final double _circleRadius = 75.0;
+  final double _circleBorderWidth = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedCard(
+      direction: AnimatedCardDirection.left,
+      initDelay: Duration(milliseconds: 0),
+      duration: Duration(seconds: 1),
+      curve: Curves.bounceOut,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Card(
+          color: Colors.white70,
+          elevation: 5,
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 280.0,
+            child: Column(
+              children: <Widget>[
+                Stack(alignment: Alignment.topCenter, children: <Widget>[
+                  Padding(
+                    child: SizedBox(
+                      width: 350.0,
+                      height: 180.0,
+                      child: Card(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
+                          child: ReadMoreText(
+                            post.description,
+                            //HomeScreen.currentAppUser.email,
+                            trimLines: 5,
+                            colorClickableText: Theme.of(context).primaryColor,
+                            trimMode: TrimMode.Line,
+                            trimCollapsedText: '...ver mas',
+                            trimExpandedText: ' ver menos',
+                          ),
+                        ),
+                      ),
+                    ),
+                    padding: EdgeInsets.only(top: _circleRadius / 2.0),
+                  ),
+                  Container(
+                    width: _circleRadius,
+                    height: _circleRadius,
+                    decoration: ShapeDecoration(shape: CircleBorder(), color: Colors.white),
+                    child: Padding(
+                      padding: EdgeInsets.all(_circleBorderWidth),
+                      child: DecoratedBox(
+                        decoration: ShapeDecoration(
+                          shape: CircleBorder(),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              post.profilePicUrl,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ]),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 35.0,
+                          width: 100.0,
+                          child: Material(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(25.0),
+                            ),
+                            elevation: 5.0,
+                            color: Colors.white,
+                            child: Container(
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 25.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Column(
+                                      children: <Widget>[
+                                        SubscribeWidget(isSubscribed: _isSubscribed, subscribedCount: _subscribedCount, onTapped: _onSubscribe),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    SizedBox(
+                      height: 35.0,
+                      width: 100.0,
+                      child: Material(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(25.0),
+                        ),
+                        elevation: 5.0,
+                        color: Colors.white,
+                        child: InkWell(
+                          onTap: () => _goToMapWithCoordinates(context, post.latitudeStarting, post.longitudeStarting, post.latitudeEnd, post.longitudeEnd),
+                          child: Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 35.0, top: 3.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.location_on,
+                                        color: Colors.grey,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
-  _goToMapWithCoordinates(
-      context, double latitudeStarting, double longitudeStarting, double latitudeEnd, double longitudeEnd) {
+  _goToMapWithCoordinates(context, double latitudeStarting, double longitudeStarting, double latitudeEnd, double longitudeEnd) {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => PostMap(latitudeStarting, longitudeStarting, latitudeEnd, longitudeEnd),
       ),
+    );
+  }
+
+  Future<bool> _onSubscribe() async {
+    if (_isSubscribed) {
+      post.assistants.remove(currentUserFullName);
+      await FirestoreManagement().addListener(post.pid, post.assistants);
+      return false;
+    } else {
+      post.assistants.add(currentUserFullName);
+      await FirestoreManagement().addListener(post.pid, post.assistants);
+      return true;
+    }
+  }
+}
+
+class SubscribeWidget extends StatelessWidget {
+  SubscribeWidget({Key key, this.isSubscribed: false, this.subscribedCount: 0, @required this.onTapped}) : super(key: key);
+
+  final bool isSubscribed;
+  final int subscribedCount;
+  final Future<bool> Function() onTapped;
+
+  Future<bool> _onTapped(bool isLiked) async {
+    return onTapped();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LikeButton(
+      likeCount: subscribedCount,
+      likeBuilder: (bool isLiked) => Icon(
+        Icons.directions_run,
+        color: isSubscribed ? Colors.deepPurpleAccent : Colors.grey,
+      ),
+      onTap: _onTapped,
     );
   }
 }
