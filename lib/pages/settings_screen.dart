c127b78b89d141ac31bda860a2a3f3447dbdfb8f
Charlie <carlos.tambascia@gmail.com>
2020-05-18 00:01:42 -0300

    Fixing styles on settings

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -20,110 +20,108 @@ class _SettingsState extends State<Settings> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topRight,
-                  child: FloatingActionButton(
-                    onPressed: () => _logout(context),
-                    materialTapTargetSize: MaterialTapTargetSize.padded,
-                    backgroundColor: Colors.red,
-                    child: Icon(Icons.exit_to_app, size: 25.0),
-                    heroTag: 'exit',
-                  ),
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topRight,
+                child: FloatingActionButton(
+                  onPressed: () => _logout(context),
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.red,
+                  child: Icon(Icons.exit_to_app, size: 25.0),
+                  heroTag: 'exit',
                 ),
-                GestureDetector(
-                  onTap: () => editing ? showDialog(context: context, builder: (_) => _askForSource()) : null,
-                  child: AvatarGlow(
-                    endRadius: 90,
-                    duration: Duration(seconds: 2),
-                    glowColor: Colors.white24,
-                    repeat: true,
-                    repeatPauseDuration: Duration(seconds: 2),
-                    startDelay: Duration(seconds: 1),
-                    child: Material(
-                        elevation: 8.0,
-                        shape: CircleBorder(),
-                        child: CircleAvatar(
-                          backgroundColor: Colors.grey[100],
-                          child: HomeScreen.currentAppUser.profilePictureUrl == "" ||
-                                  HomeScreen.currentAppUser.profilePictureUrl == null ||
-                                  editing
-                              ? Icon(
-                                  Icons.add_a_photo,
-                                  size: 35.0,
-                                  color: Colors.black54,
-                                )
-                              : CircleAvatar(
-                                  maxRadius: 90,
-                                  backgroundImage: NetworkImage(HomeScreen.currentAppUser.profilePictureUrl),
-                                ),
-                          radius: 50.0,
-                        )),
-                  ),
-                ),
-                Text(
-                  HomeScreen.currentAppUser.fullName,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontFamily: 'Lobster', fontSize: 28.0, color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                isCoach
-                    ? Container(
-                        width: 300.0,
-                        height: 300.0,
-                        child: Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: DecoratedBox(
-                            decoration: ShapeDecoration(
-                              shape: RoundedRectangleBorder(),
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: HomeScreen.currentAppUser.certificateUrl == "" ||
-                                        HomeScreen.currentAppUser.certificateUrl == null ||
-                                        editing
-                                    ? Icon(
-                                        Icons.add_a_photo,
-                                        size: 35.0,
-                                        color: Colors.black54,
-                                      )
-                                    : NetworkImage(
-                                        HomeScreen.currentAppUser.certificateUrl,
-                                      ),
+              ),
+              GestureDetector(
+                onTap: () => editing ? showDialog(context: context, builder: (_) => _askForSource()) : null,
+                child: AvatarGlow(
+                  endRadius: 90,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: HomeScreen.currentAppUser.profilePictureUrl == "" ||
+                                HomeScreen.currentAppUser.profilePictureUrl == null ||
+                                editing
+                            ? Icon(
+                                Icons.add_a_photo,
+                                size: 35.0,
+                                color: Colors.black54,
+                              )
+                            : CircleAvatar(
+                                maxRadius: 90,
+                                backgroundImage: NetworkImage(HomeScreen.currentAppUser.profilePictureUrl),
                               ),
+                        radius: 50.0,
+                      )),
+                ),
+              ),
+              Text(
+                HomeScreen.currentAppUser.fullName,
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(fontFamily: 'Lobster', fontSize: 28.0, color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              isCoach
+                  ? Container(
+                      width: 300.0,
+                      height: 300.0,
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: DecoratedBox(
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: HomeScreen.currentAppUser.certificateUrl == "" ||
+                                      HomeScreen.currentAppUser.certificateUrl == null ||
+                                      editing
+                                  ? Icon(
+                                      Icons.add_a_photo,
+                                      size: 35.0,
+                                      color: Colors.black54,
+                                    )
+                                  : NetworkImage(
+                                      HomeScreen.currentAppUser.certificateUrl,
+                                    ),
                             ),
                           ),
                         ),
-                      )
-                    : SizedBox(),
-                Visibility(
-                  visible: !editing,
-                  child: InkWell(
-                    onTap: () => setState(() => editing = true),
-                    child: EditProfileButton(
-                      buttonText: 'Editar Perfil',
-                    ),
+                      ),
+                    )
+                  : SizedBox(),
+              Visibility(
+                visible: !editing,
+                child: InkWell(
+                  onTap: () => setState(() => editing = true),
+                  child: EditProfileButton(
+                    buttonText: 'Editar Perfil',
                   ),
                 ),
-                Visibility(
-                  visible: editing,
-                  child: InkWell(
-                    onTap: () => setState(() => editing = false),
-                    child: EditProfileButton(
-                      buttonText: 'Terminado',
-                    ),
+              ),
+              Visibility(
+                visible: editing,
+                child: InkWell(
+                  onTap: () => setState(() => editing = false),
+                  child: EditProfileButton(
+                    buttonText: 'Terminado',
                   ),
                 ),
-              ],
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 88.0),
+              ),
+            ],
           ),
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 30.0),
         ),
       ),
     );
