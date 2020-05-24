ce791dfdcc331bd1bbeb4bf0fa965a86ec558b06
Charlie <carlos.tambascia@gmail.com>
2020-05-24 20:11:42 -0300

    Adding description.

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -24,133 +24,145 @@ class _SettingsState extends State<Settings> {
   final isCoach = HomeScreen.currentAppUser.isCoach;
   final _store = FirebaseStorage.instance;
   final _picId = HomeScreen.currentAppUser.picId;
+  TextEditingController textEditingController = new TextEditingController();
   bool _loading = false;
   String _description = '';
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return ModalProgressHUD(
-      child: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            child: Column(children: <Widget>[
-              Align(
-                alignment: Alignment.topRight,
-                child: FloatingActionButton(
-                  onPressed: () => _logout(context),
-                  materialTapTargetSize: MaterialTapTargetSize.padded,
-                  backgroundColor: Colors.red,
-                  child: Icon(Icons.exit_to_app, size: 25.0),
-                  heroTag: 'exit',
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color(0XFF6a54b0),
+                Color(0xFF2953a7),
+              ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+            ),
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              child: Column(children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: FloatingActionButton(
+                    onPressed: () => _logout(context),
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
+                    backgroundColor: Colors.red,
+                    child: Icon(Icons.exit_to_app, size: 25.0),
+                    heroTag: 'exit',
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () => showDialog(context: context, builder: (_) => _askForSource(true)),
-                child: AvatarGlow(
-                  endRadius: 90,
-                  duration: Duration(seconds: 2),
-                  glowColor: Colors.white24,
-                  repeat: true,
-                  repeatPauseDuration: Duration(seconds: 2),
-                  startDelay: Duration(seconds: 1),
-                  child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[100],
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(100.0),
-                          child: FadeInImage(
-                            width: 100,
-                            height: 100,
-                            placeholder: AssetImage('assets/img/defaultProfile.jpg'),
-                            image: NetworkImage(HomeScreen.currentAppUser.profilePictureUrl),
-                            fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () => showDialog(context: context, builder: (_) => _askForSource(true)),
+                  child: AvatarGlow(
+                    endRadius: 90,
+                    duration: Duration(seconds: 2),
+                    glowColor: Colors.white24,
+                    repeat: true,
+                    repeatPauseDuration: Duration(seconds: 2),
+                    startDelay: Duration(seconds: 1),
+                    child: Material(
+                        elevation: 8.0,
+                        shape: CircleBorder(),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100.0),
+                            child: FadeInImage(
+                              width: 100,
+                              height: 100,
+                              placeholder: AssetImage('assets/img/defaultProfile.jpg'),
+                              image: NetworkImage(HomeScreen.currentAppUser.profilePictureUrl),
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                        radius: 50.0,
-                      )),
+                          radius: 50.0,
+                        )),
+                  ),
                 ),
-              ),
-              Text(
-                HomeScreen.currentAppUser.fullName,
-                textAlign: TextAlign.center,
-                style:
-                    TextStyle(fontFamily: 'Lobster', fontSize: 28.0, color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 350,
-                      height: 300,
-                      child: Container(
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
-                          child: TextField(
-                            style: TextStyle(color: Colors.white),
-                            textAlign: TextAlign.center,
-                            autocorrect: true,
-                            keyboardType: TextInputType.multiline,
-                            maxLength: 260,
-                            maxLines: 8,
-                            decoration: kDescriptionDecoration.copyWith(
-                                hintText: 'Agregar informacion personal',
-                                fillColor: Colors.white,
-                                focusColor: Colors.white,
-                                hoverColor: Colors.white,
-                                hintStyle: TextStyle(color: Colors.white)),
-                            onChanged: (value) => setState(() => _description = value),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Visibility(
-                      visible: _description.isNotEmpty,
-                      child: InkWell(
-                        onTap: () => _addUserDescription(context),
-                        child: Card(
-                          color: Colors.white70,
-                          elevation: 15.0,
-                          margin: EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              ListTile(
-                                title: Text('Agregar', textAlign: TextAlign.center),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                Text(
+                  HomeScreen.currentAppUser.fullName,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Lobster', fontSize: 28.0, color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-              ),
-              isCoach
-                  ? Expanded(
-                      child: GestureDetector(
-                        onTap: () => showDialog(context: context, builder: (_) => _askForSource(false)),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 350,
+                        height: 300,
                         child: Container(
-                          color: Colors.white70,
-                          width: 300.0,
-                          height: 200.0,
-                          margin: EdgeInsets.only(top: 15.0),
-                          child: FadeInImage(
-                            placeholder: AssetImage('assets/img/defaultCertificate.png'),
-                            image: NetworkImage(HomeScreen.currentAppUser.certificateUrl),
-                            fit: BoxFit.cover,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
+                            child: TextField(
+                              style: TextStyle(color: Colors.white),
+                              textAlign: TextAlign.center,
+                              autocorrect: true,
+                              keyboardType: TextInputType.multiline,
+                              maxLength: 260,
+                              maxLines: 8,
+                              controller: textEditingController,
+                              decoration: kDescriptionDecoration.copyWith(
+                                  hintText: 'Agregar informacion personal',
+                                  fillColor: Colors.white,
+                                  focusColor: Colors.white,
+                                  hoverColor: Colors.white,
+                                  hintStyle: TextStyle(color: Colors.white)),
+                              onChanged: (value) => setState(() => _description = value),
+                            ),
                           ),
                         ),
                       ),
-                    )
-                  : SizedBox(),
-            ]),
-            padding: EdgeInsets.all(10.0),
+                      Visibility(
+                        visible: _description.isNotEmpty,
+                        child: InkWell(
+                          onTap: () => _addUserDescription(context),
+                          child: Card(
+                            color: Colors.white70,
+                            elevation: 15.0,
+                            margin: EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                ListTile(
+                                  title: Text('Agregar', textAlign: TextAlign.center),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                isCoach
+                    ? Expanded(
+                        child: GestureDetector(
+                          onTap: () => showDialog(context: context, builder: (_) => _askForSource(false)),
+                          child: Container(
+                            color: Colors.white70,
+                            width: 300.0,
+                            height: 200.0,
+                            margin: EdgeInsets.only(top: 15.0),
+                            child: FadeInImage(
+                              placeholder: AssetImage('assets/img/defaultCertificate.png'),
+                              image: NetworkImage(HomeScreen.currentAppUser.certificateUrl),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      )
+                    : SizedBox(),
+              ]),
+              padding: EdgeInsets.all(10.0),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 30.0),
           ),
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 30.0),
         ),
       ),
       inAsyncCall: _loading,
@@ -225,7 +237,13 @@ class _SettingsState extends State<Settings> {
     try {
       setState(() => _loading = true);
       await FirestoreManagement().addUserDescription(_description);
+      textEditingController.clear();
+      _description = '';
       setState(() => _loading = false);
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text('Actualizado con exito'),
+        duration: Duration(seconds: 3),
+      ));
     } catch (e) {
       setState(() {
         _loading = false;
